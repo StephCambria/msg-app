620881f4fe3f6b0258ee0a1ff7e8ca3803c6d455
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-03-09 13:43:22 -0500

    starting

---
+++ public/msg-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* I wish I knew about this syntax earlier! Greatly simplifies front-end DOM manipulation */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
