d9ace3d67ffd8408f227c35c8b01d2440301476c
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-05-04 13:06:36 -0400

    db data rendering

--- public/msg-app/src/pages/Chat.jsx
+++ public/msg-app/src/pages/Chat.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Contacts from "../components/Contacts";
+import { allUsersRoute, host } from "../utils/APIRoutes";
 
 function Chat() {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    async function fetchCurrentUser() {
+      await JSON.parse(localStorage.getItem("msg-app-user"));
+    }
+    if (!localStorage.getItem("msg-app-user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(JSON.parse(localStorage.getItem("msg-app-user")));
+    }
+    fetchCurrentUser();
+  }, []);
+
+  // -----------
+  // This is the code that finally got all of the users in the database to render!
+  useEffect(() => {
+    async function getContacts() {
+      if (currentUser) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      }
+    }
+    getContacts();
+  }, [currentUser]);
+
   return (
-    <div>Chat</div>
-  )
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} currentUser={currentUser} />
+      </div>
+    </Container>
+  );
 }
 
-export default Chat
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
+
+export default Chat;
