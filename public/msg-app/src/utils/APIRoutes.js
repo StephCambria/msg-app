cdc9c2d04070d3c6a5507d934de4bb1137f24a40
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-05-04 13:05:53 -0400

    async GET working

--- public/msg-app/src/utils/APIRoutes.js
+++ public/msg-app/src/utils/APIRoutes.js
@@ -1,3 +1,7 @@
+// -------------------
+// Front-end routing has been a game changer for me
+
 const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
