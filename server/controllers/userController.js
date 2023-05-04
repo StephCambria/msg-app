e1d7f58aa02582d07e089be88b9becca6f01e7f5
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-05-04 13:04:33 -0400

    async GET working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,3 +40,16 @@ module.exports.login = async (req, res, next) => {
     next(error);
   }
 };
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({_id:{ $ne: req.params.id }}).select([
+      "email",
+      "username",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (ex) {
+    next(ex);
+  }
+}
