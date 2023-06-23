2b05645f6ac70c555ae5cd60809e15927462a0c3
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-06-23 12:58:27 -0400

    working on input & adding messages component

--- public/msg-app/src/components/ChatInput.jsx
+++ public/msg-app/src/components/ChatInput.jsx
@@ -22,4 +22,62 @@ export default function ChatInput() {
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 5% 95%;
+  align-items: center;
+  background-color: #080420;
+  padding: 0 2rem;
+  padding-bottom: 0.3rem;
+  .button-container {
+    display: flex;
+    align-items: center;
+    color: white;
+    gap: 1rem;
+    .emoji {
+      position: relative;
+      svg {
+        font-size: 1.5rem;
+        color: #ffff00c8;
+        cursor: pointer;
+      }
+    }
+  }
+  .input-container {
+    width: 100%;
+    border-radius: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    background-color: #ffffff34;
+    input {
+      width: 90%;
+      height: 60%;
+      background-color: transparent;
+      color: white;
+      border: none;
+      padding-left: 1rem;
+      font-size: 1.2rem;
+      &::selection {
+        background-color: #9a86f3;
+      }
+      &:focus {
+        outline: none;
+      }
+    }
+    button {
+      padding: 0.3rem 2rem;
+      border-radius: 2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #9a86f3;
+      border: none;
+      cursor: pointer;
+      svg {
+        font-size: 2rem;
+        color: white;
+      }
+    }
+  }
+`;
