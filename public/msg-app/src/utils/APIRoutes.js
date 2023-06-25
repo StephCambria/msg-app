e5442dcf095f91c2e814bbb970a13cb79b54a8f5
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2023-06-25 16:41:52 -0400

    messages send to the db now

--- public/msg-app/src/utils/APIRoutes.js
+++ public/msg-app/src/utils/APIRoutes.js
@@ -2,6 +2,11 @@
 // Front-end routing has been a game changer for me
 
 const host = "http://localhost:5000";
+
+// User
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
+
+// Messages
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
