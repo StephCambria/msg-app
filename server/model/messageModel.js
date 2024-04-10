a6ef453d667b7202f5d09703bfdcac20e9f3274a
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2024-04-10 12:42:33 -0400

    HELLO input actually renders now

--- server/model/messageModel.js
+++ server/model/messageModel.js
@@ -7,14 +7,14 @@ const messageSchema = new mongoose.Schema(
         type: String,
         required: true,
       },
+    //  users: Array,
+    //  sender: {
+    //    type: mongoose.Schema.Types.ObjectId,
+   //     ref: "User",
+    //    required: true,
+      },
     },
-    users: Array,
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  },
+ // },
   {
     timestamps: true,
   }
