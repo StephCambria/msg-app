dafb6fc7a8e3a0a501fa128cfeb1d163fa6ca86f
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2024-04-10 13:21:40 -0400

    HELLO input actually renders now

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,37 +1,39 @@
 const messageModel = require("../model/messageModel");
 
-module.exports.addMessage = async (req, res, next) => {
-  try {
-    const { from, to, message } = req.body;
-    const data = await messageModel.create({
-      message: { text: message },
-      users: [from, to],
-      sender: from,
-    });
-    if (data) return res.json({ msg: "Message added!" });
-    return res.json({ msg: "Unable to add message to the database" });
-  } catch (ex) {
-    next(ex);
-  }
-};
-
 module.exports.getAllMessage = async (req, res, next) => {
   try {
     const { from, to } = req.body;
     const messages = await messageModel
       .find({
         users: {
-          $all: [from, to],
+          $all: [from, to, _id],
         },
       })
-      .sort({ updatedAt: 1 });
+      .sort({ updatedAt: 1 })
+     // .filter({ _id });
+
     const projectedMessages = messages.map((msg) => {
       return {
-        fromSelf: msg.sender.toString() === from,
+        //fromSelf: msg.sender.toString() === from,
         message: msg.message.text,
       };
     });
     res.json(projectedMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await messageModel.create({
+      message: { text: message },
+      users: [from, to],
+      //sender: from,
+    });
+    if (data) return res.json({ msg: "Message added!" });
+    return res.json({ msg: "Unable to add message to the database" });
   } catch (ex) {
     next(ex);
   }
